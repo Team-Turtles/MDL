d13100721f9073f8956c1924bca280562b23fe9a
Turtles-Michelangelo <Turtles-Michelangelo@mikza.video@gmail.com>
2014-10-06 16:27:32 +0200

    test version 55555555

--- MaisonDesLigues/Bdd.cs
+++ MaisonDesLigues/Bdd.cs
@@ -19,7 +19,7 @@ namespace BaseDeDonnees
         //Awoulagagtoubib
         // propriétés membres
         //
-        private int test2;
+        private int test21;
         private SqlConnection cn;
         private SqlCommand UneSqlCommand;
         private SqlDataAdapter UnSqlDataAdapter;
